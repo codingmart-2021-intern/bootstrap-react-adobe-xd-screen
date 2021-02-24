f008fef96a7aac1c0074069259821a7441466335
BalajiRagupathi <1999bala10@gmail.com>
2021-02-24 10:24:49 +0530

    header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,47 @@
-import { Container } from "react-bootstrap";
-import HeaderTop from './header-top/HeaderTop';
-import Navbar from './navbar/Navbar';
+import React, { Component } from "react";
+import "./Header.css";
+import HeaderTop from "./header-top/HeaderTop";
+import Navbar from "./navbar/Navbar";
+import MobileViewHeader from "./mobileViewHeader/MobileViewHeader";
 
-const Header = () => {
-  return (
-    <Container fluid className="bg-danger p-4">
-      <HeaderTop />
-      <Navbar />
-    </Container>
-  );
-};
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sideBarToggleState: false,
+    };
+
+    this.SideBarToggel = this.SideBarToggel.bind(this);
+  }
+
+  SideBarToggel() {
+    this.setState((preState) => {
+      return { sideBarToggleState: !preState.sideBarToggleState };
+    });
+    console.log(this.state.sideBarToggleState);
+  }
+
+  render() {
+    return (
+      <div className="light-pink p-0 m-0">
+        <div className="desktop-view-header">
+          <div className="w-90 mx-auto">
+            <HeaderTop />
+          </div>
+          <hr></hr>
+          <div className="w-90 mx-auto">
+            <Navbar />
+          </div>
+        </div>
+        <div className="mobile-view-header">
+          <MobileViewHeader
+            sideBarToggle={this.SideBarToggel}
+            sideBarToggleState={this.state.sideBarToggleState}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Header;
