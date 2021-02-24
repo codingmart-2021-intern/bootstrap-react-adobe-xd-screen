77b27c6ce2b22cdd6bbb8c81f5d48fd139050b71
Dinesh I <dineshiyyandhurai@gmail.com>
2021-02-24 15:55:42 +0530

    Changes in Slider

--- src/components/home/Features/Features.js
+++ src/components/home/Features/Features.js
@@ -33,27 +33,27 @@ const Features = () => {
         <div className="w-75 mx-auto">
             <h1 className="text-center fw-bold fs-3 my-5">Featured Product</h1>
             <div className="d-flex justify-content-center align-items-center position-relative">
-                <div onClick={() => changeSlide(-1)} className="z-10 p-2 h-8 w-8 bg-skin_dark fs-3 rounded-circle d-flex justify-content-center align-items-center pointer-event text-white position-absolute top-50 start-0">
+                <div onClick={() => changeSlide(-1)} className="z-10 h-2 w-2 bg-skin_dark fs-3 rounded-circle d-flex justify-content-center align-items-center pointer-event text-white position-absolute top-50 start-0">
                     <i className='bx bxs-chevron-left' ></i>
                 </div>
                 <div className="row">
                     {
                         productDataList.map(productData => (
-                            <Link className="col" to={ "/detail/" + productData.product_id }>
-                                <div className="d-flex flex-column gap-2 " key={productData.product_id}>
-                                    <div className="position-relative">
-                                        <img className="w-10 h-20 object-cover" src={productData.slider_image[0].img} alt={productData.product_title} />
+                            <Link className="card-c col" to={ "/flowers/" + productData.product_id }>
+                                <div className="d-flex flex-column gap-2 w-10" key={productData.product_id}>
+                                    <div className="position-relative d-flex justify-content-center align-items-center ">
+                                        <img className="w-10 h-12 object-cover" src={productData.slider_image[0].img} alt={productData.product_title} />
                                         {   productData.stock == 0 && 
-                                            <div className="w-100 h-100 bg-white position-absolute top-0 d-flex justify-content-center align-items-center">
-                                                <button className="py-2 px-4 bg-footer_gray text-skin_dark">OUT OF STOCK</button>
+                                            <div className="w-100 h-100 bg-white-5 position-absolute d-flex justify-content-center align-items-center">
+                                                <button className="py-2 px-3 bg-footer_gray text-skin_dark">OUT OF STOCK</button>
                                             </div>
                                         }
                                     </div>
-                                    <div className="px-3 py-1 d-flex flex-column gap-2 bg-skin_light">
-                                        <h3>{productData.product_title}</h3>
+                                    <div className="px-3 py-1 d-flex flex-column gap-2 bg-skin_light text-dark">
+                                        <span>{productData.product_title}</span>
                                         <div className="d-flex gap-2">
-                                            <span className="fw-bold fs-8"> &#8377; {productData.current_price}</span>
-                                            <span className="fw-normal fs-8 text-decoration-line-through"> &#8377; {productData.actual_price}</span>
+                                            <span className="fw-bold fs-7"> &#8377; {productData.current_price}</span>
+                                            <span className="fs-7 text-decoration-line-through"> &#8377; {productData.actual_price}</span>
                                         </div>
                                     </div>
                                 </div>
@@ -61,7 +61,7 @@ const Features = () => {
                         ))
                     }
                 </div>
-                <div onClick={() => changeSlide(1)} className="z-10 p-2 h-8 w-8 bg-skin_dark fs-3 rounded-circle d-flex justify-content-center align-items-center pointer-event text-white position-absolute top-50 start-0">
+                <div onClick={() => changeSlide(1)} className="z-10 h-2 w-2 bg-skin_dark fs-3 rounded-circle d-flex justify-content-center align-items-center pointer-event text-white position-absolute top-50 end-0">
                     <i className='bx bxs-chevron-right' ></i>
                 </div>
             </div>
